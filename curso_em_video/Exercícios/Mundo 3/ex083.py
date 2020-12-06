c3fcc4659fa2c50a258b14867d6bcd22fd0fa4ff
nralex <alexserno@gmail.com>
2020-12-06 20:56:17 -0300

    Esse eu não dei conta sozinho, mas o raciocínio é simples

--- curso_em_video/Exercícios/Mundo 3/ex083.py
+++ curso_em_video/Exercícios/Mundo 3/ex083.py
@@ -1,2 +1,17 @@
 '''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. 
 Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
+expressão = str(input('Digite a expressão: '))
+lista = []
+for símbolo in expressão:
+    if símbolo == '(':
+        lista.append('(')
+    elif símbolo == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print('Sua expressão está válida')
+else:
+    print('Sua expressão está errada')
