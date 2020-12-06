d105054b3d91aaee1447eb0a7cb633547259a5c4
nralex <alexserno@gmail.com>
2020-12-06 18:58:47 -0300

    listas na ordem, estudo de Python

--- curso_em_video/Aulas/Aula17a.py
+++ curso_em_video/Aulas/Aula17a.py
@@ -2,7 +2,7 @@ n =  [2, 5, 9, 1]
 n[2] = 3
 n.append(7)
 n.sort(reverse=True)
-n.insert(2,2)
+n.insert(2,3)
 #n.pop(2)
 if 5 in n:
     n.remove(5)
---
+++ curso_em_video/Aulas/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if 5 in n:
+    n.remove(5)
+else:
+    print('Não achei o n° 4')
