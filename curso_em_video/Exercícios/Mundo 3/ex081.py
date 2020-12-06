2b275ba616f975d38f5dce775499378bc6c6733d
nralex <alexserno@gmail.com>
2020-12-06 19:21:12 -0300

    Curso em vídeo exercício de Python3 081

--- curso_em_video/Exercícios/Mundo 3/ex081.py
+++ curso_em_video/Exercícios/Mundo 3/ex081.py
@@ -2,3 +2,21 @@
 A) Quantos números foram digitados.
 B) A lista de valores, ordenada de forma decrescente.
 C) Se o valor 5 foi digitado e está ou não na lista.'''
+l = []
+c = 0
+while True:
+    c += 1
+    n = int(input('Digite um valor: '))
+    l.append(n)
+    o = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if o not in 'SN':
+        o = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if o == 'N':
+        break
+l.sort(reverse=True)
+print(f'Você digitou {c} elementos.')    
+print(f'Os valores em ordem decrescentes são {l}')
+if 5 in l:
+    print('O valor 5 faz parte da lista')
+else:
+    print('O valor 5 não faz parte da lista')
