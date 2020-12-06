fee0b763f18ff3a7cbd174cc897f43cb175e7089
nralex <alexserno@gmail.com>
2020-12-06 17:04:07 -0300

    trabalhando no ex080

--- curso_em_video/Exercícios/Mundo 3/ex080.py
+++ curso_em_video/Exercícios/Mundo 3/ex080.py
@@ -1,2 +1,5 @@
 '''Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, 
 já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.'''
+numéro = list()
+for c in range(0,5):
+    numéro.append(input('Digite um número: '))
--- curso_em_video/Exercícios/Mundo 3/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-print(f'O maior valor digitado foi {mai} nas posições ')
-print(f'O menor valor digitado foi {men} nas posições ')
