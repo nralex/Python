7ea607e7678a3b1dd8519512a81f16593c8d3709
nralex <alexserno@gmail.com>
2020-12-10 20:47:56 -0300

    Exercício 84

--- curso_em_video/Aulas/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-if 5 in n:
-    n.remove(5)
-else:
-    print('Não achei o n° 4')
--- curso_em_video/Exercícios/Mundo 3/ex084.py
+++ curso_em_video/Exercícios/Mundo 3/ex084.py
@@ -1,7 +1,34 @@
-"""
-Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:
-
+""" Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:
 A) Quantas pessoas foram cadastradas.
 B) Uma listagem com as pessoas mais pesadas.
 C) Uma listagem com as pessoas mais leves.
 """
+pessoas = []
+grupo = []
+maior = menor = 0
+while True:
+    pessoas.append(str(input('Nome: ')))
+    pessoas.append(int(input('Peso: ')))
+    if len(grupo) == 0:
+        maior = menor = pessoas[1]
+    else:
+        if pessoas[1] > maior:
+            maior = pessoas[1]
+        if pessoas[1] < menor:
+            menor = pessoas[1]
+    opção = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if opção not in 'SN':
+        opção = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    grupo.append(pessoas[:])
+    pessoas.clear()
+    if opção in 'N':
+        break
+print(f'Ao todo você cadastrou {len(grupo)} pessoas')
+print(f'O maior peso foi {maior}kg', end=' ')
+for p in grupo:
+    if p[1] == maior:
+        print(f'{p[0]}', end=' ')
+print(f'\nO menor peso foi {menor}kg', end=' ')
+for p in grupo:
+    if p[1] == menor:
+        print(f'{p[0]}', end=' ')
