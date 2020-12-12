d0150bdab5fe0bf2115b3a3852e9f14ab874334a
nralex <alexserno@gmail.com>
2020-12-12 14:56:15 -0300

    Exercícios 88 e 89

--- curso_em_video/Exercícios/Mundo 3/ex089b.py
+++ curso_em_video/Exercícios/Mundo 3/ex089b.py
@@ -1,24 +1,11 @@
-"""
-Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta.
-No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as
-notas de cada aluno individualmente.
-"""
 from time import sleep
 lista = []
-aluno = []
-notas = []
-c = 0
 while True:
     nome = str(input('Nome: ')).title().strip()
-    for n in range(1, 3):
-        nota = float(input(f'Nota {n}: '))
-        notas.append(nota)
-    aluno.append(nome)
-    aluno.append(notas[:])
-    lista.append(aluno[:])
-    aluno.clear()
-    notas.clear()
-    c += 1
+    nota1 = float(input(f'Nota 1: '))
+    nota2 = float(input(f'Nota 2: '))
+    média = (nota1 + nota2) / 2
+    lista.append([nome, [nota1, nota2], média])
     opção = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
     while opção not in 'SN':
         opção = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
@@ -31,8 +18,7 @@ listarNome = 0
 for l in range(0,len(lista)):
     print(f'{f"{l}":4}', end='')
     print(f'{lista[l][0]:40}', end='')
-    média = (lista[l][1][0] + lista[l][1][1]) / 2
-    print(f'{média:>5.1f}')
+    print(f'{lista[l][2]:>5.1f}')
 print('-' * 50)
 while True:
     qualAluno = int(input('Mostrar a nota de qual aluno? (999 interrompe): '))
