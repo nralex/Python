dd51ea5dc45e65c898bac31e72fc7d6cc6283101
nralex <alexserno@gmail.com>
2020-12-13 14:21:11 -0300

    Exercício 90

--- curso_em_video/Exercícios/Mundo 3/ex090.py
+++ curso_em_video/Exercícios/Mundo 3/ex090.py
@@ -1,2 +1,12 @@
 '''Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
 No final, mostre o conteúdo da estrutura na tela.'''
+aluno = {}
+aluno['nome'] = str(input('Nome: ')).title().strip()
+aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['média'] < 7:
+    aluno['situação'] = 'Reprovado'
+else:
+    aluno['situação'] = 'Aprovado'
+print(f'Nome é igual a {aluno["nome"]}')
+print(f'Média é igual a {aluno["média"]}')
+print(f'Situação é igual a {aluno["situação"]}')
