236bfeb230e268431039f5c6e53aa0390b679885
nralex <alexserno@gmail.com>
2020-12-15 13:37:01 -0300

    Aula 20

---
+++ curso_em_video/Aulas/Aula20a.py
@@ -0,0 +1,23 @@
+def soma(a, b):
+    s = a + b
+    print(s)
+
+
+# Programa principal
+soma(4, 5)
+soma(8, 9)
+soma(2, 1)
+
+# Resume
+a = 4
+b = 5
+s = a + b
+print(s)
+a = 8
+b = 9
+s = a + b
+print(s)
+a = 2
+b = 1
+s = a + b
+print(s)
---
+++ curso_em_video/Aulas/Aula20b.py
@@ -0,0 +1,9 @@
+def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma de A + B = {s}')
+
+
+# Programa principal
+soma(4, 5)
+soma(b=4, a=5)
