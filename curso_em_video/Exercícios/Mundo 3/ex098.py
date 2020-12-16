bea7d3bd5dad68f832032c53590be9a95ea53d5c
nralex <alexserno@gmail.com>
2020-12-15 21:10:32 -0300

    Exercício 98

--- curso_em_video/Exercícios/Mundo 3/ex098.py
+++ curso_em_video/Exercícios/Mundo 3/ex098.py
@@ -10,15 +10,32 @@ c) Uma contagem personalizada.
 from time import sleep
 
 
-def contador(a, b, c):
+def contador(início, fim, passo):
+    if passo < 0:
+        passo *= -1
+    if passo == 0:
+        passo = 1
     print('-=' * 20)
-    print(f'Contagem de {a} até {b} de {c} em {c}')
-    for a in range(a, b + 1):
-        #sleep(1)
-        print(a, end=' ')
-        a += c
-    print()
+    print(f'Contagem de {início} até {fim} de {passo} em {passo}')
+    sleep(2)
+    if início < fim:
+        contador = início
+        while contador <= fim:
+            print(contador, end=' ')
+            sleep(0.5)
+            contador += passo
+        print()
+
+    if início > fim:
+        contador = início
+        while contador >= fim:
+            print(contador, end=' ')
+            sleep(0.5)
+            contador -= passo
+        print()
 
 
-#contador(1, 10, 1)
+contador(1, 10, 1)
 contador(10, 0, 2)
+print('Agora é a sua vez de personalizar a contagem!')
+contador(int(input(f'{"Início":8}')), int(input(f'{"Fim:":8}')), int(input(f'{"Passo:":8}')))
