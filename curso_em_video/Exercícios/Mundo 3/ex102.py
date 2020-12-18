7a161eb324474559b3f749e76687d4cfa1366b99
nralex <alexserno@gmail.com>
2020-12-18 20:50:31 -0300

    Exercício 102

--- curso_em_video/Exercícios/Mundo 3/ex102.py
+++ curso_em_video/Exercícios/Mundo 3/ex102.py
@@ -3,3 +3,18 @@ Crie um programa que tenha uma função fatorial() que receba dois parâmetros: 
 número a calcular e o outro chamado show, que será um valor lógico (opcional) indicando se será
 mostrado ou não na tela o processo de cálculo do fatorial.
 """
+
+def fatorial(número=1, show=False):
+    print('-' * 30)
+    f = 1
+    for c in range(número, 0, -1):
+        f *= c
+        if show == True:
+            if c == 1:
+                print(c, end=' = ')
+            else:
+                print(c, end=' x ')
+    return f
+
+
+print(fatorial(5, True))
