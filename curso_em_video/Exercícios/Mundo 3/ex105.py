74ac1a4a0cbf69c1a7a009332aab564444ec852a
nralex <alexserno@gmail.com>
2020-12-21 20:47:33 -0300

    Exercício 105

--- curso_em_video/Exercícios/Mundo 3/ex105.py
+++ curso_em_video/Exercícios/Mundo 3/ex105.py
@@ -10,3 +10,31 @@ retornar um dicionário com as seguintes informações:
 
 Adicione também as docstrings da função.
 """
+def notas(*num, sit=False):
+    """
+    notas(*num, sit=False)
+        -> Função para analizar notas e situação de vários alunos.
+        :parametro num: uma ou mais notas dos alunos (aceita vários)
+        :parametro sit: valor opcional, indicando se deve ou não mostrar a situação.
+        :return: dicionário com várias informações sobre a situação da turma.
+    """
+    dicionário = dict()
+    dicionário['total'] = len(num)
+    dicionário['maior'] = max(num)
+    dicionário['menor'] = min(num)
+    med = sum(num) / len(num)
+    dicionário['média'] = med
+    if sit:
+        if med < 5:
+            situação = 'RUIM'
+        if 5 <= med < 7:
+            situação = 'RAZOÁVEL'
+        if med >= 7:
+            situação = 'BOA'
+        dicionário['situação'] = situação
+    return dicionário
+
+
+resposta = notas(5.5, 2.5, 10, 6.5, sit = False)
+print(resposta)
+help(notas)
