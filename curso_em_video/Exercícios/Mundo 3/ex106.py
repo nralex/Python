575974c351c8d6cec39305916bfd7345c3a40543
nralex <alexserno@gmail.com>
2020-12-22 11:16:42 -0300

    Exercício 106

--- curso_em_video/Exercícios/Mundo 3/ex106.py
+++ curso_em_video/Exercícios/Mundo 3/ex106.py
@@ -4,3 +4,38 @@ manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se en
 
 OBS: use cores.
 """
+from time import sleep
+c = ('\033[m',          # 0 - Sem cor
+    '\033[0;30;41m',    # 1 - Vermelho
+    '\033[0;30;42m',    # 2 - Verde
+    '\033[0,30,43m',    # 3 - Amarelo
+    '\033[0,30,44m',    # 4 - Azul
+    '\033[0,30,45m',    # 5 - Roxo
+    '\033[7;30m'        # 6 - Branco
+    )
+
+
+def título(menságem, cor=0):
+    tamanho = len(menságem) + 4
+    print(c[cor], end='')
+    print('~' * tamanho)
+    print(f'  {menságem}')
+    print('~' * tamanho)
+    print(c[0], end='')
+    sleep(1)
+
+
+def ajuda(com):
+    título(f'Acessando a manual do comando \'{com}\'', 4)
+    help(com)        
+
+
+comando = ''
+while True:
+    título('SISTEMA DE AJUDA PyHELP', 2)
+    comando = str(input('Função ou Biblioteca > '))
+    if comando.upper() == 'FIM':
+        título('ATÉ LOGO!', 1)
+        break
+    else:
+        ajuda(comando)
