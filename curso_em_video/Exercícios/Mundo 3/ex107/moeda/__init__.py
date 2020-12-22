2d6cbd75aee7230dc99f0ea1ae4c62ffc2d4ff4a
nralex <alexserno@gmail.com>
2020-12-22 19:47:08 -0300

    Exercício 107

---
+++ curso_em_video/Exercícios/Mundo 3/ex107/moeda/__init__.py
@@ -0,0 +1,15 @@
+def metade(n):
+    return n / 2
+
+
+def dobro(n):
+    return n * 2
+
+
+def aumentar(n, porcentagem):
+
+    return n * (1 + (porcentagem / 100))
+
+
+def diminuir(n, porcentagem):
+    return n * (1 - (porcentagem / 100))
