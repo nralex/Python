66d6b972d149bf9a69a199e504b3d1906a00a82f
NRAlex <alexserno@gmail.com>
2020-12-23 08:27:25 -0300

    nada não

--- curso_em_video/Exercícios/Mundo 3/ex108/moeda.py
+++ curso_em_video/Exercícios/Mundo 3/ex108/moeda.py
@@ -1,19 +1,19 @@
-def metade(n = 0):
+def metade(n=0):
     return n / 2
 
 
-def dobro(n = 0):
+def dobro(n=0):
     return n * 2
 
 
-def aumentar(n = 0, porcentagem = 0):
+def aumentar(n=0, porcentagem=0):
 
     return n * (1 + (porcentagem / 100))
 
 
-def diminuir(n = 0, porcentagem = 0):
+def diminuir(n=0, porcentagem=0):
     return n * (1 - (porcentagem / 100))
 
 
-def moeda(n = 0, moeda = 'R$'):
+def moeda(n=0, moeda='R$'):
     return f'{moeda}{n:.2f}'.replace('.', ',')
