4574eaa9276750b414f7dd68c0bf12d2bd228129
NRAlex <alexserno@gmail.com>
2020-12-23 09:10:24 -0300

    Exercício 110 corrigido

--- curso_em_video/Exercícios/Mundo 3/ex110/moeda.py
+++ curso_em_video/Exercícios/Mundo 3/ex110/moeda.py
@@ -1,34 +1,34 @@
 def metade(n, formatado=False):
     valor = n / 2
-    return valor if formatado is False else moeda(n)
+    return valor if formatado is False else moeda(valor)
 
 
 def dobro(n, formatado=False):
     valor = n * 2
-    return valor if formatado is False else moeda(n)
+    return valor if formatado is False else moeda(valor)
 
 
 def aumentar(n, porcentagem, formatado=False):
     valor = n * (1 + (porcentagem / 100)) 
-    return valor if formatado is False else moeda(n)
+    return valor if formatado is False else moeda(valor)
 
 
 def diminuir(n, porcentagem, formatado=False):
     valor = n * (1 - (porcentagem / 100)) 
-    return valor if formatado is False else moeda(n)
+    return valor if formatado is False else moeda(valor)
 
 
 def moeda(n=0, moeda='R$'):
     return f'{moeda}{n:.2f}'.replace('.', ',')
 
 
-def resumo(n, aum, red):
+def resumo(n=0, aum=10, red=5):
     print('-' * 40)
-    print(f'{"RESUMO DO VALOR":^40}')
+    print('RESUMO DO VALOR'.center(40))
     print('-' * 40)
     print(f'{"Preço analisado:":25}', f'{moeda(n):>14}')
     print(f'{"Dobro do preço:":25}', f'{dobro(n, True):>14}')
     print(f'{"Metade do preço:":25}', f'{metade(n, True):>14}')
-    print(f'{f"{aum}% de aumento:":25}', f'{aumentar(n, aum):>14}')
-    print(f'{f"{red}% de redução:":25}', f'{diminuir(n, red):>14}')
+    print(f'{f"{aum}% de aumento:":25}', f'{aumentar(n, aum, True):>14}')
+    print(f'{f"{red}% de redução:":25}', f'{diminuir(n, red, True):>14}')
     print('-' * 40)
