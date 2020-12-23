77bdbc9fe1115cf6fd7bf77a1832ddb3e1912464
NRAlex <alexserno@gmail.com>
2020-12-23 09:47:06 -0300

    Exercício 112 (Entendi, mas não dei conta)

--- curso_em_video/Exercícios/Mundo 3/ex112/ex112.py
+++ curso_em_video/Exercícios/Mundo 3/ex112/ex112.py
@@ -6,3 +6,7 @@ Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo cham
 Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função input(),
 mas com uma validação de dados para aceitar apenas valore monetários.
 """
+from utilidadesCeV import moeda, dado
+
+p = dado.leiaDinheiro('Digite o preço: R$')
+moeda.resumo(p, 60, 30)
